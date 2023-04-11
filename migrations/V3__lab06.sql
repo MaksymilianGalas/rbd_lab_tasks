c8670bddc2acd6a53ce12c849da869c2c0835884
MaksymilianGalas <122981146+MaksymilianGalas@users.noreply.github.com>
2023-04-11 19:37:31 +0200

    Update V3__lab06.sql

--- migrations/V3__lab06.sql
+++ migrations/V3__lab06.sql
@@ -4,3 +4,42 @@ WHERE pickup_address_id > 1
 GROUP BY driver_id
 HAVING COUNT(*) >= 1
 ORDER BY AVG(distance) DESC;
+
+
+
+
+SELECT * FROM data.user
+RIGHT JOIN data.address ON area_id = user_id
+LIMIT 5 OFFSET 5;
+
+SELECT * FROM data.user
+left join data.address ON area_id = user_id
+
+SELECT * FROM data.user
+RIGHT JOIN data.address ON area_id = user_id
+
+SELECT * FROM data.user
+inner join data.address ON area_id = user_id
+
+
+SELECT * FROM data.user
+full outer join data.address ON area_id = user_id
+
+
+SELECT user_surname FROM data."user"
+UNION
+SELECT user_surname FROM data.user
+
+
+SELECT user_id FROM data.user
+EXCEPT
+SELECT passenger_id FROM data.passenger_rating
+
+
+
+SELECT driver_id FROM data.driver_rating
+WHERE car_condition > 3
+INTERSECT
+SELECT driver_id FROM data.driver_rating
+WHERE complying_with_regulations > 3
+
